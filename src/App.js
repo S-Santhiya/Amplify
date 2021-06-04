39d78a143a3b77124253414cb93658f667ee0412
unknown <santhiya@1cloudhub.com>
2021-06-04 12:00:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        New React Application
+        New React Applications
       </header>
     </div>
   );
